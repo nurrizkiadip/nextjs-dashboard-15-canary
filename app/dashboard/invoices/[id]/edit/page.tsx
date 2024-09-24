dae827848bdbfa70c7bb0c0e337a9db53d59254c
nurrizkiadip <nrizki@dicoding.com>
2024-09-24 13:57:34 +0700

    Complete chapter 13: handling errors

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,6 +1,7 @@
 import Form from '@/app/ui/invoices/edit-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { fetchCustomers, fetchInvoiceById } from '@/app/lib/data';
+import { notFound } from 'next/navigation';
 
 interface EditPageParams {
   params: {
@@ -15,6 +16,10 @@ export default async function Page({ params }: EditPageParams) {
     fetchInvoiceById(id),
     fetchCustomers(),
   ]);
+
+  if (!invoice) {
+    notFound();
+  }
 
   return (
     <main>
