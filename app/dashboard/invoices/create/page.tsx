7c228efb322fe4455c90b03ec3f3786acbb65edf
nurrizkiadip <nrizki@dicoding.com>
2024-09-24 13:32:03 +0700

    Complete chapter 12: mutating data

---
+++ app/dashboard/invoices/create/page.tsx
@@ -0,0 +1,30 @@
+import { fetchCustomers } from '@/app/lib/data';
+import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
+import Form from '@/app/ui/invoices/create-form';
+
+interface CreateInvoiceProps {
+  searchParams?: {
+    query?: string;
+    page?: string;
+  };
+}
+
+export default async function Page({}: CreateInvoiceProps) {
+  const customers = await fetchCustomers();
+
+  return (
+    <main>
+      <Breadcrumbs
+        breadcrumbs={[
+          {label: 'Invoices', href: '/dashboard/invoices'},
+          {
+            label: 'Create Invoice',
+            href: '/dashboard/invoices/create',
+            active: true,
+          },
+        ]}
+      />
+      <Form customers={customers}/>
+    </main>
+  );
+}
