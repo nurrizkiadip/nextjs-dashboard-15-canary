762251b2330f4088b9e26fd35e54321baec385b0
nurrizkiadip <nrizki@dicoding.com>
2024-09-25 10:41:13 +0700

    Complete chapter 16: adding metadata

--- app/dashboard/invoices/create/page.tsx
+++ app/dashboard/invoices/create/page.tsx
@@ -1,6 +1,11 @@
 import { fetchCustomers } from '@/app/lib/data';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import Form from '@/app/ui/invoices/create-form';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Create Invoices',
+};
 
 interface CreateInvoiceProps {
   searchParams?: {
